60a1e205fb8e673c1dcbd48dfe3cec53d2688fbb
deceitya <49361292+deceitya@users.noreply.github.com>
2025-01-18 11:41:48 +0900

    nullable

--- src/main/kotlin/mc/fuckoka/economyapi/event/PlayerPaidMoneyEvent.kt
+++ src/main/kotlin/mc/fuckoka/economyapi/event/PlayerPaidMoneyEvent.kt
@@ -5,13 +5,18 @@ import mc.fuckoka.economyapi.model.Wallet
 import org.bukkit.event.Event
 import org.bukkit.event.HandlerList
 
-class PlayerPaidMoneyEvent(val from: Wallet, val to: Wallet, val amount: Money) : Event() {
+class PlayerPaidMoneyEvent(val from: Wallet?, val to: Wallet?, val amount: Money) : Event() {
     companion object {
         private val handlers = HandlerList()
 
         fun getHandlerList(): HandlerList {
             return handlers
         }
+    }
+
+    init {
+        // どちらかは必ず存在すること
+        require(from != null || to != null)
     }
 
     override fun getHandlers(): HandlerList = handlers
