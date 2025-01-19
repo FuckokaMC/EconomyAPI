bb903480cce4cbf5f48e60ac70da4c68d01e1c73
deceitya <49361292+deceitya@users.noreply.github.com>
2025-01-19 13:25:37 +0900

    ビルドが失敗するのでplugin.ymlを書く

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,7 @@
-import net.minecrell.pluginyml.bukkit.BukkitPluginDescription
-
 plugins {
     kotlin("jvm") version "2.0.21"
     `maven-publish`
-    id("com.github.johnrengelman.shadow") version "7.1.2"
-    id("net.minecrell.plugin-yml.bukkit") version "0.5.2"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
 group = "mc.fuckoka"
@@ -22,6 +19,7 @@ repositories {
 
 dependencies {
     compileOnly("io.papermc.paper:paper-api:${mcVersion}-R0.1-SNAPSHOT")
+    compileOnly("com.mysql:mysql-connector-j:9.1.0")
     testImplementation(kotlin("test"))
 }
 
@@ -47,78 +45,6 @@ publishing {
             register<MavenPublication>("gpr") {
                 from(components["java"])
             }
-        }
-    }
-}
-
-bukkit {
-    name = "EconomyAPI"
-    version = getVersion().toString()
-    description = "経済プラグイン"
-    author = "deceitya"
-    main = "mc.fuckoka.economyapi.EconomyAPI"
-    apiVersion = mcVersion.substring(0, mcVersion.lastIndexOf("."))
-    load = BukkitPluginDescription.PluginLoadOrder.POSTWORLD
-
-    commands {
-        register("mymoney") {
-            description = "自分の所持金を表示する。"
-            permission = "economyapi.commands.mymoney"
-            usage = "/mymoney"
-        }
-        register("paymoney") {
-            description = "プレイヤーにお金を支払う。"
-            permission = "economyapi.commands.paymoney"
-            usage = "/paymoney <プレイヤー名> <金額> [理由]"
-        }
-        register("showmoney") {
-            description = "プレイヤーのお金を表示する。"
-            permission = "economyapi.commands.showmoney"
-            usage = "/showmoney <プレイヤー名>"
-        }
-        register("setmoney") {
-            description = "プレイヤーの所持金を設定する。"
-            permission = "economyapi.commands.setmoney"
-            usage = "/setmoney <プレイヤー名> <金額> [理由]"
-        }
-        register("givemoney") {
-            description = "プレイヤーの所持金を増やす。"
-            permission = "economyapi.commands.givemoney"
-            usage = "/givemoney <プレイヤー名> <金額> [理由]"
-        }
-        register("takemoney") {
-            description = "プレイヤーの所持金を減らす。"
-            permission = "economyapi.commands.takemoney"
-            usage = "/takemoney <プレイヤー名> <金額> [理由]"
-        }
-        register("logmoney") {
-            description = "所持金のログを表示する。"
-            permission = "economyapi.commands.logmoney"
-            usage = "/logmoney [ページ]"
-        }
-    }
-
-    permissions {
-        register("economyapi.commands.mymoney") {
-            default = BukkitPluginDescription.Permission.Default.TRUE
-        }
-        register("economyapi.commands.paymoney") {
-            default = BukkitPluginDescription.Permission.Default.OP
-        }
-        register("economyapi.commands.showmoney") {
-            default = BukkitPluginDescription.Permission.Default.OP
-        }
-        register("economyapi.commands.setmoney") {
-            default = BukkitPluginDescription.Permission.Default.OP
-        }
-        register("economyapi.commands.givemoney") {
-            default = BukkitPluginDescription.Permission.Default.OP
-        }
-        register("economyapi.commands.takemoney") {
-            default = BukkitPluginDescription.Permission.Default.OP
-        }
-        register("economyapi.commands.logmoney") {
-            default = BukkitPluginDescription.Permission.Default.TRUE
         }
     }
 }
