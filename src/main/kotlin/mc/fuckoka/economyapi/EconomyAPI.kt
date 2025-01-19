7f945f252dd274ec966986b0ded4e7a1ca7634f2
deceitya <49361292+deceitya@users.noreply.github.com>
2025-01-19 17:15:37 +0900

    disableでDB解放

--- src/main/kotlin/mc/fuckoka/economyapi/EconomyAPI.kt
+++ src/main/kotlin/mc/fuckoka/economyapi/EconomyAPI.kt
@@ -1,6 +1,10 @@
 package mc.fuckoka.economyapi
 
+import mc.fuckoka.economyapi.config.Database
 import org.bukkit.plugin.java.JavaPlugin
 
 class EconomyAPI : JavaPlugin() {
+    override fun onDisable() {
+        Database.close()
+    }
 }
