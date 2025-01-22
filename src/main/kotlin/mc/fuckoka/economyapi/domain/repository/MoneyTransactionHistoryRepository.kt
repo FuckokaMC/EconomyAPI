4b12feebea0c1ff1fcb1e651161641ffddc26085
deceitya <49361292+deceitya@users.noreply.github.com>
2025-01-22 21:38:39 +0900

    WalletIDからも探すように

--- src/main/kotlin/mc/fuckoka/economyapi/domain/repository/MoneyTransactionHistoryRepository.kt
+++ src/main/kotlin/mc/fuckoka/economyapi/domain/repository/MoneyTransactionHistoryRepository.kt
@@ -4,6 +4,8 @@ import mc.fuckoka.economyapi.domain.model.MoneyTransaction
 import java.util.*
 
 interface MoneyTransactionHistoryRepository {
+    fun find(wallet: Long): List<MoneyTransaction>
+
     fun findBy(player: UUID): List<MoneyTransaction>
 
     fun store(event: MoneyTransaction)
