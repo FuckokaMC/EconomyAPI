8e18f22e1d716a6ccbb7f4c14b1e7eb4ded07dfa
deceitya <49361292+deceitya@users.noreply.github.com>
2025-01-26 00:35:05 +0900

    理由つき

--- src/main/kotlin/mc/fuckoka/economyapi/application/GiveMoneyUseCase.kt
+++ src/main/kotlin/mc/fuckoka/economyapi/application/GiveMoneyUseCase.kt
@@ -2,6 +2,7 @@ package mc.fuckoka.economyapi.application
 
 import mc.fuckoka.dbconnector.Database
 import mc.fuckoka.economyapi.domain.model.Money
+import mc.fuckoka.economyapi.domain.model.Reason
 import mc.fuckoka.economyapi.domain.repository.MoneyTransactionHistoryRepository
 import mc.fuckoka.economyapi.domain.repository.WalletRepository
 import java.util.UUID
@@ -10,14 +11,14 @@ class GiveMoneyUseCase(
     private val walletRepository: WalletRepository,
     private val historyRepository: MoneyTransactionHistoryRepository
 ) {
-    fun execute(player: UUID, amount: Int): Boolean {
+    fun execute(player: UUID, amount: Int, reason: String? = null): Boolean {
         return Database.transaction {
             val wallet = walletRepository.findBy(player) ?: return@transaction false
 
             val money = Money(amount)
             // お金を受け取れない場合はfalseを返す
             if (!wallet.canCredited(money)) return@transaction false
-            val moneyTransaction = wallet.credited(Money(amount))
+            val moneyTransaction = wallet.credited(Money(amount), if (reason != null) Reason(reason) else null)
 
             historyRepository.store(moneyTransaction)
             walletRepository.store(wallet)
--- src/main/kotlin/mc/fuckoka/economyapi/application/TakeMoneyUseCase.kt
+++ src/main/kotlin/mc/fuckoka/economyapi/application/TakeMoneyUseCase.kt
@@ -2,6 +2,7 @@ package mc.fuckoka.economyapi.application
 
 import mc.fuckoka.dbconnector.Database
 import mc.fuckoka.economyapi.domain.model.Money
+import mc.fuckoka.economyapi.domain.model.Reason
 import mc.fuckoka.economyapi.domain.repository.MoneyTransactionHistoryRepository
 import mc.fuckoka.economyapi.domain.repository.WalletRepository
 import java.util.UUID
@@ -10,14 +11,14 @@ class TakeMoneyUseCase(
     private val walletRepository: WalletRepository,
     private val historyRepository: MoneyTransactionHistoryRepository
 ) {
-    fun execute(player: UUID, amount: Int): Boolean {
+    fun execute(player: UUID, amount: Int, reason: String? = null): Boolean {
         return Database.transaction {
             val wallet = walletRepository.findBy(player) ?: return@transaction false
 
             val money = Money(amount)
             // お金を支払えない場合はfalseを返す
             if (!wallet.canPay(money)) return@transaction false
-            val moneyTransaction = wallet.pay(money)
+            val moneyTransaction = wallet.pay(money, if (reason != null) Reason(reason) else null)
 
             historyRepository.store(moneyTransaction)
             walletRepository.store(wallet)
