e175a6f9ef46c53065e7f0133799c3bcad237d25
deceitya <49361292+deceitya@users.noreply.github.com>
2025-01-26 00:21:33 +0900

    お金を増やすユースケース

---
+++ src/main/kotlin/mc/fuckoka/economyapi/application/GiveMoneyUseCase.kt
@@ -0,0 +1,23 @@
+package mc.fuckoka.economyapi.application
+
+import mc.fuckoka.dbconnector.Database
+import mc.fuckoka.economyapi.domain.model.Money
+import mc.fuckoka.economyapi.domain.repository.MoneyTransactionHistoryRepository
+import mc.fuckoka.economyapi.domain.repository.WalletRepository
+import java.util.UUID
+
+class GiveMoneyUseCase(
+    private val walletRepository: WalletRepository,
+    private val historyRepository: MoneyTransactionHistoryRepository
+)  {
+    fun execute(player: UUID, amount: Int) {
+        Database.transaction {
+            val wallet = walletRepository.findBy(player) ?: throw RuntimeException()
+
+            val moneyTransaction = wallet.credited(Money(amount))
+
+            historyRepository.store(moneyTransaction)
+            walletRepository.store(wallet)
+        }
+    }
+}
