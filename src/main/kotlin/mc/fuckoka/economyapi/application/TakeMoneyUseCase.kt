1bd3cb5dfcf349b501114b51dbdc71f2ef158255
deceitya <49361292+deceitya@users.noreply.github.com>
2025-01-26 00:29:21 +0900

    お金支払い

---
+++ src/main/kotlin/mc/fuckoka/economyapi/application/TakeMoneyUseCase.kt
@@ -0,0 +1,28 @@
+package mc.fuckoka.economyapi.application
+
+import mc.fuckoka.dbconnector.Database
+import mc.fuckoka.economyapi.domain.model.Money
+import mc.fuckoka.economyapi.domain.repository.MoneyTransactionHistoryRepository
+import mc.fuckoka.economyapi.domain.repository.WalletRepository
+import java.util.UUID
+
+class TakeMoneyUseCase(
+    private val walletRepository: WalletRepository,
+    private val historyRepository: MoneyTransactionHistoryRepository
+) {
+    fun execute(player: UUID, amount: Int): Boolean {
+        return Database.transaction {
+            val wallet = walletRepository.findBy(player) ?: return@transaction false
+
+            val money = Money(amount)
+            // お金を支払えない場合はfalseを返す
+            if (!wallet.canPay(money)) return@transaction false
+            val moneyTransaction = wallet.pay(money)
+
+            historyRepository.store(moneyTransaction)
+            walletRepository.store(wallet)
+
+            return@transaction true
+        }
+    }
+}
