30e266bf174219f66ce9bbbbb31a5096ac2f71f2
deceitya <49361292+deceitya@users.noreply.github.com>
2025-02-20 23:42:26 +0900

    リポジトリ実装

---
+++ src/main/kotlin/mc/fuckoka/economyapi/infrastructure/WalletRepositoryImpl.kt
@@ -0,0 +1,43 @@
+package mc.fuckoka.economyapi.infrastructure
+
+import mc.fuckoka.dbconnector.Database
+import mc.fuckoka.economyapi.domain.model.Wallet
+import mc.fuckoka.economyapi.domain.repository.WalletRepository
+import java.sql.SQLException
+
+abstract class WalletRepositoryImpl : WalletRepository {
+    override fun store(wallet: Wallet) {
+        val connection = Database.connection ?: throw SQLException()
+        val stmt = connection.prepareStatement(
+            """
+                |SELECT
+                |    1
+                |FROM wallets
+                |WHERE wallets.id = ?
+                |LIMIT 1
+            """.trimMargin()
+        )
+        val exists = stmt.use {
+            stmt.setLong(1, wallet.id.value)
+            val resultSet = stmt.executeQuery()
+            resultSet.use {
+                resultSet.next()
+            }
+        }
+        if (exists) {
+            val stmt1 = connection.prepareStatement("INSERT INTO wallets (owner, money) VALUES (UUID_TO_BIN(?), ?)")
+            stmt1.use {
+                stmt1.setString(1, wallet.owner.toString())
+                stmt1.setInt(2, wallet.money.value)
+                stmt.executeUpdate()
+            }
+        } else {
+            val stmt1 = connection.prepareStatement("UPDATE wallets SET money = ? WHERE id = ?")
+            stmt1.use {
+                stmt1.setInt(1, wallet.money.value)
+                stmt1.setLong(2, wallet.id.value)
+                stmt.executeUpdate()
+            }
+        }
+    }
+}
