f2d09e6a4f5c8b3e57d2b3495cc4952dbe898ad3
deceitya <49361292+deceitya@users.noreply.github.com>
2025-02-20 23:14:27 +0900

    ビュー不要

--- src/main/resources/table.sql
+++ src/main/resources/table.sql
@@ -14,11 +14,3 @@ CREATE TABLE money_transactions (
     FOREIGN KEY (player) REFERENCES wallets(id),
     FOREIGN KEY (payee) REFERENCES wallets(id)
 );
-
-CREATE VIEW current_moneys AS
-SELECT
-    player,
-    -- 支払先として受け取った金額の合計値 - 支払った金額の合計
-    (SELECT SUM(amount) FROM money_transactions AS mt2 WHERE mt2.payee = mt1.player) - SUM(amount) AS money
-FROM money_transactions AS mt1
-GROUP BY player;
