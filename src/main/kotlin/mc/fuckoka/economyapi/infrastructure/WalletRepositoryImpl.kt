3a9cbd9e835408432fa1baa689f5a47ab74efa71
deceitya <49361292+deceitya@users.noreply.github.com>
2025-03-11 00:33:37 +0900

    単純なWalletRepository実装

--- src/main/kotlin/mc/fuckoka/economyapi/infrastructure/WalletRepositoryImpl.kt
+++ src/main/kotlin/mc/fuckoka/economyapi/infrastructure/WalletRepositoryImpl.kt
@@ -1,43 +1,87 @@
 package mc.fuckoka.economyapi.infrastructure
 
 import mc.fuckoka.dbconnector.Database
+import mc.fuckoka.economyapi.domain.model.Money
 import mc.fuckoka.economyapi.domain.model.Wallet
+import mc.fuckoka.economyapi.domain.model.WalletID
 import mc.fuckoka.economyapi.domain.repository.WalletRepository
 import java.sql.SQLException
+import java.util.*
 
-abstract class WalletRepositoryImpl : WalletRepository {
-    override fun store(wallet: Wallet) {
+open class WalletRepositoryImpl : WalletRepository {
+    override fun find(id: WalletID): Wallet? {
         val connection = Database.getConnection() ?: throw SQLException()
-        // UUIDで存在確認しているのはUUIDがユニークであるため(idで確認してもuuidが重複する可能性があるため)
         val stmt = connection.prepareStatement(
             """
                 |SELECT
-                |    1
+                |    id,
+                |    BIN_TO_UUID(owner) AS owner,
+                |    money
                 |FROM wallets
-                |WHERE wallets.owner = UUID_TO_BIN(?)
-                |LIMIT 1
+                |WHERE id = ?
             """.trimMargin()
         )
-        val exists = stmt.use {
-            stmt.setString(1, wallet.owner.toString())
-            val resultSet = stmt.executeQuery()
-            resultSet.use {
-                resultSet.next()
+        stmt.use {
+            stmt.setInt(1, id.value)
+
+            val result = stmt.executeQuery()
+            result.use {
+                if (result.next()) {
+                    return Wallet(
+                        WalletID(result.getInt("id")),
+                        UUID.fromString(result.getString("owner")),
+                        Money(result.getInt("money"))
+                    )
+                }
             }
         }
-        if (exists) {
-            val stmt1 = connection.prepareStatement("UPDATE wallets SET money = ? WHERE id = ?")
-            stmt1.use {
-                stmt1.setInt(1, wallet.money.value)
-                stmt1.setInt(2, wallet.id.value)
+        return null
+    }
+
+    override fun findBy(owner: UUID): Wallet? {
+        val connection = Database.getConnection() ?: throw SQLException()
+        val stmt = connection.prepareStatement(
+            """
+                |SELECT
+                |    id,
+                |    BIN_TO_UUID(owner) AS owner,
+                |    money
+                |FROM wallets
+                |WHERE owner = UUID_TO_BIN(?)
+            """.trimMargin()
+        )
+        stmt.use {
+            stmt.setString(1, owner.toString())
+
+            val result = stmt.executeQuery()
+            result.use {
+                if (result.next()) {
+                    return Wallet(
+                        WalletID(result.getInt("id")),
+                        UUID.fromString(result.getString("owner")),
+                        Money(result.getInt("money"))
+                    )
+                }
+            }
+        }
+        return null
+    }
+
+    override fun store(wallet: Wallet) {
+        val connection = Database.getConnection() ?: throw SQLException()
+        if (wallet is Wallet.NewWallet) {
+            val stmt = connection.prepareStatement("INSERT INTO wallets (owner, money) VALUES (UUID_TO_BIN(?), ?)")
+            stmt.use {
+                stmt.setString(1, wallet.owner.toString())
+                stmt.setInt(2, wallet.money.value)
                 stmt.executeUpdate()
             }
         } else {
-            val stmt1 = connection.prepareStatement("INSERT INTO wallets (owner, money) VALUES (UUID_TO_BIN(?), ?)")
-            stmt1.use {
-                stmt1.setString(1, wallet.owner.toString())
-                stmt1.setInt(2, wallet.money.value)
-                stmt1.executeUpdate()
+            val stmt = connection.prepareStatement("UPDATE wallets SET money = ? WHERE id = ?")
+            stmt.use {
+                stmt.setInt(1, wallet.money.value)
+                stmt.setInt(2, wallet.id.value)
+                stmt.executeUpdate()
             }
         }
     }
